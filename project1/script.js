3627d078b436070b5ca071dfb37b63d24dd24e8d
armanahad6207 <armanahad7549@gmail.com>
2025-02-05 12:49:19 +0530

    add project 1

---
+++ project1/script.js
@@ -0,0 +1,25 @@
+
+gsap.from("nav h3",{
+    y:-50,
+    opacity:0,
+    duration:.5,
+    stagger:.2
+})
+
+gsap.from(".intro h1",{
+    x:-500,
+    duration:1,
+    delay:1,
+    opacity:0,
+    stagger:.3
+})
+
+gsap.from(".picture img",{
+    y:-100,
+    duration:.8,
+    delay:2.5,
+    opacity:0,
+    stagger:.8
+})
+
+
